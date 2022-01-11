4adf7064715cbd253765313e54fda7e604f42030
JonibekQamarov <jonibek.qamarov@bk.ru>
2022-01-11 16:45:42 +0500

    Var to const

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
-var elSiteHeaderToggler = document.querySelector(".site-header__toggler");
-var elSitenav = document.querySelector(".sitenav");
+const elSiteHeaderToggler = document.querySelector(".site-header__toggler");
+const elSitenav = document.querySelector(".sitenav");
 elSiteHeaderToggler.addEventListener("click", function () {
   elSitenav.classList.toggle("sitenav--open");
 });
